068b012b5cca8ec90be5dd140c6ee69b69fda850
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-06 12:51:29 +0300

    add assets for landing view

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:makanges_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MakanGesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
