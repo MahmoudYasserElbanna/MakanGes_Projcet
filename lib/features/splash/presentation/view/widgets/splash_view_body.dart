c44d585e9d56e12590d3508d862bfdccbbadf365
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-06 14:10:07 +0300

    remove unused packages

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:makanges_app/core/utils/app_router.dart';
 import 'package:makanges_app/core/utils/assets.dart';
