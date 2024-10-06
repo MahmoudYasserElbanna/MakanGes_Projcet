50087af69340304705202a1fe3c1e916524a3159
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-06 21:52:12 +0300

    add new images

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:makanges_app/core/utils/assets.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/custom_coupon_offer_card.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/home_view_app_bar.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/search_bar.dart';
@@ -10,7 +13,7 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildHomeViewAppBar(),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
@@ -18,6 +21,34 @@ class HomeViewBody extends StatelessWidget {
             SizedBox(height: 24),
             CustomCouponOfferCard(),
             SizedBox(height: 24),
+            Expanded(
+              child: GridView.builder(
+                shrinkWrap: true,
+                itemCount: 8,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF22A641).withOpacity(0.15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Center(
+                      child: SvgPicture.asset(
+                        Assets.sushiIcon,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
