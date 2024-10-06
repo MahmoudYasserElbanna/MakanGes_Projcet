d6134434738c30f37a83e32e6a48bc8dab49f155
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-06 22:16:44 +0300

    create grid item

---
+++ lib/features/home/presentation/view/widgets/grid_view_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:makanges_app/core/utils/styles.dart';
+
+class GridViewItem extends StatelessWidget {
+  const GridViewItem({
+    super.key,
+    required this.image,
+    required this.title,
+  });
+  final String image;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: const Color(0xFF22A641).withOpacity(0.3),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            Center(
+              child: Image.asset(image),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: Styles.textStyle12,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/widgets.dart';
 import 'package:makanges_app/core/utils/assets.dart';
+import 'package:makanges_app/core/utils/styles.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/custom_coupon_offer_card.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/home_view_app_bar.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/search_bar.dart';
@@ -12,7 +14,7 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildHomeViewAppBar(),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
@@ -20,33 +22,56 @@ class HomeViewBody extends StatelessWidget {
             SizedBox(height: 24),
             CustomCouponOfferCard(),
             SizedBox(height: 24),
-            Expanded(
-              child: GridView.builder(
-                shrinkWrap: true,
-                itemCount: 8,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                ),
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          const Color(0xFF22A641).withOpacity(0.15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: Center(
-                      child: Image.asset(Assets.drinksIcon),
-                    ),
-                  ),
-                ),
-              ),
-            )
+            CustomGridView()
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomGridView extends StatelessWidget {
+  const CustomGridView({
+    super.key,
+  });
+
+  final List<String> images = const [
+    Assets.burgerIcon,
+    Assets.drinksIcon,
+    Assets.pizzaIcon,
+    Assets.meatIcon,
+    Assets.sushiIcon,
+    Assets.promoIcon,
+    Assets.tacoIcon,
+    Assets.moreIcon,
+  ];
+
+  final List<String> titles = const [
+    'Burger',
+    'Drinks',
+    'Pizza',
+    'Meat',
+    'Sushi',
+    'Promo',
+    'Taco',
+    'More',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        shrinkWrap: true,
+        itemCount: 8,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        itemBuilder: (context, index) => GridViewItem(
+          image: images[index],
+          title: titles[index],
         ),
       ),
     );
