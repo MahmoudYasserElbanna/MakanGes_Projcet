615361c9eb98ca5e4d1b094f9520890a7d845dce
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-06 22:19:01 +0300

    refactor search bar

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/custom_coupon_offer_card.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/custom_grid_view.dart';
 import 'package:makanges_app/features/home/presentation/view/widgets/home_view_app_bar.dart';
-import 'package:makanges_app/features/home/presentation/view/widgets/search_bar.dart';
+import 'package:makanges_app/features/home/presentation/view/widgets/custom_search_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
