a2393f71b25afb076514347659cfa9f9d6bbca95
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-06 13:17:51 +0300

    add images

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,15 @@
+import 'dart:html';
+
 abstract class Assets {
-  static const String splash = 'assets/images/splash.png';
+  static const String logo = 'assets/images/SplashViewIcon.svg';
+  static const String burgerIcon = 'assets/images/Burger_Icon.svg';
+  static const String drinksIcon = 'assets/images/Drink_Icon.svg';
+  static const String meatIcon = 'assets/images/Meat_Icon.svg';
+  static const String moreIcon = 'assets/images/More_Icon.svg';
+  static const String pizzaIcon = 'assets/images/Pizza_Icon.svg';
+  static const String promoIcon = 'assets/images/Promo_Icon.svg';
+  static const String sushiIcon = 'assets/images/Sushi_Icon.svg';
+  static const String tacoIcon = 'assets/images/Taco_Icon.svg';
+  static const String pizzaImage = 'assets/images/Pizza_Image.svg';
+  static const String burgerImage = 'assets/images/burger_Image.svg';
 }
