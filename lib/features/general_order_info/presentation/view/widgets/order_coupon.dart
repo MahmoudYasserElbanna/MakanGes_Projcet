dacfb013b24adc6df7d53c7db4f68ecdfc532a5d
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-18 17:09:24 +0300

    navigate to order in details view

--- lib/features/general_order_info/presentation/view/widgets/order_coupon.dart
+++ lib/features/general_order_info/presentation/view/widgets/order_coupon.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:makanges_app/core/utils/app_router.dart';
 import 'package:makanges_app/core/utils/styles.dart';
 import 'package:makanges_app/core/widgets/custom_elevated_button.dart';
 
