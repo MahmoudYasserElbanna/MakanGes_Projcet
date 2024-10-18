cdf94c2495cce35bda1b896473ce5b44c439371e
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-18 19:14:31 +0300

    add my cart view

---
+++ lib/features/checkout/presentation/view/my_cart_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:makanges_app/features/checkout/presentation/view/widgets/my_cart_view_body.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyCartViewBody();
+  }
+}
