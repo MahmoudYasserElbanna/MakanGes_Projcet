b3bf8b4fa010766b3ad9c1545420de8ed9006540
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-18 19:15:39 +0300

    add my cart view route

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:makanges_app/features/checkout/presentation/view/my_cart_view.dart';
 import 'package:makanges_app/features/general_order_info/presentation/view/general_order_view.dart';
 import 'package:makanges_app/features/home/presentation/view/home_view.dart';
 import 'package:makanges_app/features/order_in_details/presentation/view/order_in_details_view.dart';
@@ -9,6 +10,7 @@ abstract class AppRouters {
   static const String homeViewRoute = '/homeView';
   static const String generalOrderInfoViewRoute = '/generalOrderInfoViewRoute';
   static const String orderInDetailsViewRoute = '/orderInDetailsViewRoute';
+  static const String myCartViewRoute = '/myCartViewRoute';
   static GoRouter router = GoRouter(
     routes: [
       GoRoute(
@@ -26,7 +28,11 @@ abstract class AppRouters {
       GoRoute(
         path: orderInDetailsViewRoute,
         builder: (context, state) => const OrderInDetailsView(),
-      )
+      ),
+      GoRoute(
+        path: myCartViewRoute,
+        builder: (context, state) => const MyCartView(),
+      ),
     ],
   );
 }
