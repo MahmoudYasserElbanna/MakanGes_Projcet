1dd80a62f89b880472b1da1ff3af87b84c6f5e60
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-22 21:04:59 +0300

    finish stripe repo

---
+++ lib/features/checkout/presentation/data/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:makanges_app/core/errors/failure.dart';
+import 'package:makanges_app/features/checkout/presentation/data/models/stripe_payment_intent_model/stripe_payment_intent_model.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment({
+    required StripePaymentIntentModel stripePaymentIntentInputModel,
+  });
+}
