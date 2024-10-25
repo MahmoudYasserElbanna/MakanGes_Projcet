cba9bbc4ed2cd6a6f9e988d2033003cc3f042962
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-25 21:42:09 +0300

    add publishable key

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:makanges_app/core/utils/api_keys.dart';
 import 'package:makanges_app/core/utils/app_router.dart';
 
 void main() {
+  Stripe.publishableKey = ApiKeys.publishableKey;
   runApp(const MakanGesApp());
 }
 
