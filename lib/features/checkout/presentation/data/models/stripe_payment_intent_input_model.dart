3544633bb508ac261f995f2e454b0ef96d68bee6
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-31 11:52:15 +0300

    add customer id to input model

--- lib/features/checkout/presentation/data/models/stripe_payment_intent_input_model.dart
+++ lib/features/checkout/presentation/data/models/stripe_payment_intent_input_model.dart
@@ -1,16 +1,19 @@
 class StripePaymentIntentInputModel {
   final String currency;
   final int amount;
+  String? customerId;
 
   StripePaymentIntentInputModel({
     required this.amount,
     required this.currency,
+    this.customerId,
   });
 
   toJson() {
     return {
       'amount': amount * 100,
       'currency': currency,
+      'customer': customerId,
     };
   }
 }
