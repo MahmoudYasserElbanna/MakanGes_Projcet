57a7905350951a54bca1ec82b25fc3425e283034
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-05 13:08:43 +0200

    add delivery view

--- lib/features/checkout/presentation/view/delivery_info_view.dart
+++ lib/features/checkout/presentation/view/delivery_info_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:makanges_app/features/checkout/presentation/view/widgets/delivery_info_view_body.dart';
 
 class DeliveryInfoView extends StatelessWidget {
   const DeliveryInfoView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const DeliveryInfoViewBody();
   }
 }
---
+++ lib/features/checkout/presentation/view/widgets/delivery_info_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeliveryInfoViewBody extends StatelessWidget {
+  const DeliveryInfoViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
