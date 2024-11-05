27180d0dcff6c2ccdf51cd672e24f0aba2df43cf
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-05 17:46:47 +0200

    refactor code

--- lib/features/delivery/presentation/view/widgets/progress_line.dart
+++ lib/features/delivery/presentation/view/widgets/progress_line.dart
@@ -10,7 +10,7 @@ class ProgressLine extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 2,
-      width: 55,
+      width: MediaQuery.of(context).size.width * 0.14,
       color: isCompletedStage ? Colors.green : Colors.grey,
     );
   }
