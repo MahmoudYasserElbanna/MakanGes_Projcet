7282c7f4829e903e5bde43b1b7ddf7c80d61272a
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-06 14:10:36 +0200

    update UI

--- lib/features/general_order_info/presentation/view/widgets/general_order_view_header_icons.dart
+++ lib/features/general_order_info/presentation/view/widgets/general_order_view_header_icons.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:makanges_app/core/widgets/custom_icon_button.dart';
+import 'package:share_plus/share_plus.dart';
 
 class GeneralOrderViewHeaderIcons extends StatelessWidget {
   const GeneralOrderViewHeaderIcons({
@@ -25,7 +26,12 @@ class GeneralOrderViewHeaderIcons extends StatelessWidget {
             ),
             CustomIconButton(
               icon: Icons.share_outlined,
-              onPressed: () {},
+              onPressed: () {
+                Share.share(
+                  'Check out my order! Sharing my meal details with you 🍽️',
+                  subject: 'Order Details Using MakanGes Application',
+                );
+              },
             ),
             CustomIconButton(
               icon: Icons.info_outline_rounded,
