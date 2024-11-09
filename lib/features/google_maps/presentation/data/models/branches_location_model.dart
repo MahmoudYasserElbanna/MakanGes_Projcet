56ddcb0ef086ff0755e8a702c25df2bad814429b
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-09 21:59:57 +0200

    refactor code

--- lib/features/google_maps/presentation/data/models/branches_location_model.dart
+++ lib/features/google_maps/presentation/data/models/branches_location_model.dart
@@ -1,31 +1,31 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class BranchesLocationModel {
-  final String branchName;
-  final int branchId;
-  final LatLng locationLatLng;
+  final String name;
+  final int id;
+  final LatLng latLng;
 
   BranchesLocationModel({
-    required this.branchName,
-    required this.branchId,
-    required this.locationLatLng,
+    required this.name,
+    required this.id,
+    required this.latLng,
   });
 }
 
 List<BranchesLocationModel> branchesLocation = [
   BranchesLocationModel(
-    branchName: 'Cairo Branch',
-    branchId: 1,
-    locationLatLng: const LatLng(30.02351656255252, 31.24355470740745),
+    name: 'Cairo Branch',
+    id: 1,
+    latLng: const LatLng(30.02351656255252, 31.24355470740745),
   ),
   BranchesLocationModel(
-    branchName: 'Alex Branch',
-    branchId: 2,
-    locationLatLng: const LatLng(31.47377193783098, 31.120716294418443),
+    name: 'Alex Branch',
+    id: 2,
+    latLng: const LatLng(31.47377193783098, 31.120716294418443),
   ),
   BranchesLocationModel(
-    branchName: 'Giza Branch',
-    branchId: 3,
-    locationLatLng: const LatLng(30.012129625376858, 31.213767765424265),
+    name: 'Giza Branch',
+    id: 3,
+    latLng: const LatLng(30.012129625376858, 31.213767765424265),
   ),
 ];
